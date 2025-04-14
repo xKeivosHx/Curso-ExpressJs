20dfd62647d52b32a83b8efee9b57d6f23302f9d
Kevin Valencia Cruz <24264026+xKeivosHx@users.noreply.github.com>
2025-04-13 21:03:24 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Express JS</h1>
+    <p>This is an node.js app with express.js</p>
+    <p>Running on PORT ${PORT}</p>
+    `);
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
