d38c499724f3be5423612a047c590457a6bc5bce
Kevin Valencia Cruz <24264026+xKeivosHx@users.noreply.github.com>
2025-04-13 21:36:56 -0600

    Se elimina body parser y se agregan rutas de prueba

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +12,24 @@ app.get("/", (req, res) => {
     <h1>Express JS</h1>
     <p>This is an node.js app with express.js</p>
     <p>Running on PORT ${PORT}</p>
+    `);
+});
+
+app.get("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  console.log(userId);
+
+  res.send(`The user id is ${userId}`);
+});
+
+app.get("/search", (req, res) => {
+  const terms = req.query.term || "Not Specified";
+  const category = req.query.category || "Todas";
+
+  res.send(`
+        <h2>Resultados de la Busqueda</h2>
+        <p>Término: ${terms}</p>
+        <p>Categoría: ${category}</p>
     `);
 });
 
